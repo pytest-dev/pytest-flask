9631b6bcc2601aa30d75038d518d1472dc7a0c0a
Vital Kudzelka <vital.kudzelka@gmail.com>
2014-09-04 14:02:47 +0300

    Set custom response class in test environment
    Custom response class has `json` method to help test JSON responses:
    
        def test_json(client):
            res = client.get(url_for('ping'))
            assert res.json == {'ping': 'pong'}

--- pytest_flask/plugin.py
+++ pytest_flask/plugin.py
@@ -8,9 +8,58 @@
 """
 import pytest
 
+from flask import json
+from werkzeug import cached_property
+
 from .fixtures import (
     client, config, accept_json, accept_jsonp, accept_any, accept_mimetype
 )
+
+
+class JSONResponse(object):
+    """Mixin with testing helper methods for JSON responses."""
+
+    @cached_property
+    def json(self):
+        """Try to deserialize response data (a string containing a valid JSON
+        document) to a Python object by passing it to the underlying
+        :mod:`flask.json` module.
+        """
+        return json.loads(self.data)
+
+
+def _make_test_response_class(response_class):
+    """Extends the response class with special attribute to test JSON
+    responses. Don't override user-defined `json` attribute if any.
+
+    :param response_class: An original response class.
+    """
+    if 'json' in response_class.__dict__:
+        return response_class
+
+    return type(str(JSONResponse), (response_class, JSONResponse), {})
+
+
+@pytest.fixture(autouse=True)
+def _monkeypatch_response_class(request, monkeypatch):
+    """Set custom response class before test suite and restore the original
+    after. Custom response has `json` property to easily test JSON responses::
+
+        @app.route('/ping')
+        def ping():
+            return jsonify(ping='pong')
+
+        def test_json(client):
+            res = client.get(url_for('ping'))
+            assert res.json == {'ping': 'pong'}
+
+    """
+    if 'app' not in request.fixturenames:
+        return
+
+    app = request.getfuncargvalue('app')
+    monkeypatch.setattr(app, 'response_class',
+                        _make_test_response_class(app.response_class))
 
 
 @pytest.fixture(autouse=True)

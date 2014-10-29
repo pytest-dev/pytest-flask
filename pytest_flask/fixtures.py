7d3786ec9e46fe5cb4383f17f946757ba867da4c
Vital Kudzelka <vital.kudzelka@gmail.com>
2014-10-29 21:47:44 +0300

    Add `live_server` fixture
    Useful with Selenium and headless browsers.

--- pytest_flask/fixtures.py
+++ pytest_flask/fixtures.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import time
 import pytest
+import multiprocessing
+try:
+    from urllib2 import urlopen
+except ImportError:
+    from urllib.request import urlopen
 
 
 @pytest.yield_fixture
@@ -29,6 +35,62 @@ def client_class(request, client):
     """
     if request.cls is not None:
         request.cls.client = client
+
+
+class LiveServer(object):
+    """The helper class uses to manage live server. Handles creation and
+    stopping application in a separate process.
+
+    :param app: The application to run.
+    :param port: The port to run application.
+    """
+
+    def __init__(self, app, port):
+        self.app = app
+        self.port = port
+        self._process = None
+        self.start()
+
+    def start(self):
+        """Start application in a separate process."""
+        worker = lambda app, port: app.run(port=port, use_reloader=False)
+        self._process = multiprocessing.Process(
+            target=worker,
+            args=(self.app, self.port)
+        )
+        self._process.start()
+
+        # We must wait for the server to start listening with a maximum
+        # timeout of 5 seconds.
+        timeout = 5
+        while timeout > 0:
+            time.sleep(1)
+            try:
+                urlopen(self.url)
+                timeout = 0
+            except:
+                timeout -= 1
+
+    @property
+    def url(self):
+        """Returns the url of the test server."""
+        return 'http://localhost:%s' % self.port
+
+    def stop(self):
+        """Stop application process."""
+        if self._process:
+            self._process.terminate()
+
+    def __repr__(self):
+        return '<LiveServer listening at %s>' % self.url
+
+
+@pytest.fixture(scope='session')
+def live_server(request, app):
+    """Run application in a separate process."""
+    server = LiveServer(app, 5001)
+    request.addfinalizer(server.stop)
+    return server
 
 
 @pytest.fixture

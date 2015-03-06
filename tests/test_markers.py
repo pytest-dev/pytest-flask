d388fc2ad4a776c0e5157f09fb8f59270c99d2ed
Vital Kudzelka <vital.kudzelka@gmail.com>
2015-03-06 19:54:33 +0300

    Use monkeypath fixture to teardown app config
    Closes #27.

--- tests/test_markers.py
+++ tests/test_markers.py
@@ -2,6 +2,15 @@
 # -*- coding: utf-8 -*-
 import pytest
 
+from flask import Flask
+
+
+@pytest.fixture(scope='session')
+def app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = '42'
+    return app
+
 
 class TestAppMarker:
 
@@ -12,3 +21,6 @@ class TestAppMarker:
     @pytest.mark.app(foo=42)
     def test_update_application_config(self, config):
         assert config['FOO'] == 42
+
+    def test_application_config_teardown(self, config):
+        assert 'FOO' not in config

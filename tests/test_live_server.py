7a5f7509ce60c1e56c0ed1bb28172df5a2ded48c
Jad Kik <jadkik94@gmail.com>
2016-11-15 18:26:44 +0200

    Add test for live server with pytest-cov plugin and clean stop

--- tests/test_live_server.py
+++ tests/test_live_server.py
@@ -58,6 +58,53 @@ class TestLiveServer:
         result.stdout.fnmatch_lines(['*PASSED*'])
         assert result.ret == 0
 
+    def test_clean_stop_live_server(self, appdir):
+        pytest_cov = pytest.importorskip("pytest_cov")
+        appdir.create_test_module('''
+            import pytest
+            try:
+                from urllib2 import urlopen
+            except ImportError:
+                from urllib.request import urlopen
+
+            from flask import url_for
+
+            def test_a(live_server):
+                @live_server.app.route('/')
+                def index():
+                    return 'got it', 200
+
+                live_server.start()
+
+                res = urlopen(url_for('index', _external=True))
+                assert res.code == 200
+                assert b'got it' in res.read()
+        ''')
+        result_with = appdir.runpytest('-v',
+                               '--no-start-live-server',
+                               '--live-server-clean-stop',
+                               '--cov=%s' % str(appdir.tmpdir),
+                               '--cov-report=term-missing')
+        result_without = appdir.runpytest('-v',
+                               '--no-start-live-server',
+                               '--no-live-server-clean-stop',
+                               '--cov=%s' % str(appdir.tmpdir),
+                               '--cov-report=term-missing')
+
+        def _get_missing(r):
+            for line in r.outlines:
+                if not line.startswith('TOTAL'):
+                    continue
+                # Columns: Name   Stmts   Miss   Cover   Missing
+                return int(line.split()[2])
+            raise ValueError("Expected a TOTAL line in the cov output")
+
+        # Read the "Missing" column (i.e. lines not covered)
+        missing_with, missing_without = _get_missing(result_with), _get_missing(result_without)
+
+        # If the clean stop worked, the single line in the view function should be covered
+        assert missing_with == (missing_without - 1)
+
     def test_add_endpoint_to_live_server(self, appdir):
         appdir.create_test_module('''
             import pytest

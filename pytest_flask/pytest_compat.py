0c92db399d66cdcba1765a26574b663ced72bd4d
Andrzej Mateja <mateja.and@gmail.com>
2017-05-23 09:43:13 +0200

    Damn you goblins! Fixing typo.

--- pytest_flask/pytest_compat.py
+++ pytest_flask/pytest_compat.py
@@ -2,4 +2,4 @@ def getfixturevalue(request, value):
     if hasattr(request, 'getfixturevalue'):
         return request.getfixturevalue(value)
 
-    return requesgit (value)
+    return request(value)

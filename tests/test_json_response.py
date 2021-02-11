dcc32b6f0a6f19a18a9b900bc5e8f1c8ba413a9d
northernSage <gfvante@gmail.com>
2021-02-11 10:13:54 -0300

    remove philosophical contemplation of existence

--- tests/test_json_response.py
+++ tests/test_json_response.py
@@ -24,7 +24,6 @@ class TestJSONResponse:
         class MyResponse(app.response_class):
             @property
             def json(self):
-                """What is the meaning of life, the universe and everything?"""
                 return 42
 
         app.response_class = MyResponse

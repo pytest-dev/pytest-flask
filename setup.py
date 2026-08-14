22120c079d83c619403c67714a72405423889013
Irina <ourTusenka@yandex.ru>
2026-08-14 04:29:33 +0300

    198: remove deprecated setuptools' tests_require (#202)

--- setup.py
+++ setup.py
@@ -3,17 +3,15 @@ from pathlib import Path
 
 from setuptools import setup
 
-tests_require = []
 requirements = Path("requirements/main.txt").read_text(encoding="UTF-8").splitlines()
 extras_require = {
     "docs": Path("requirements/docs.txt").read_text(encoding="UTF-8").splitlines(),
-    "tests": [tests_require],
+    "tests": [],
 }
 
 setup(
     # Dependencies are here for GitHub's dependency graph.
     use_scm_version={"write_to": "src/pytest_flask/_version.py"},
     install_requires=requirements,
-    tests_require=tests_require,
     extras_require=extras_require,
 )
